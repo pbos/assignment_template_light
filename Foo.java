38c5e201d87a3d2ecfecfc6f56922523191f631e
Peter Boström <pbos@kth.se>
2011-09-21 20:49:46 +0200

    Testing line wrapping

--- Foo.java
+++ Foo.java
@@ -1,8 +1,9 @@
 public class Foo {
-  public Foo(){}
+ 	public Foo(){}
 
-  public static void main(String[] args){
-    System.out.println("Räksmörgås!");
-  }
+	// This is a very long line which should be forced to wrap if this thing is set up correctly. I really hope it works because whenever code escapes and remains outside the page it's really hard to read.
+ 	public static void main(String[] args) {
+		System.out.println("Räksmörgås!");
+	}
 }
 
